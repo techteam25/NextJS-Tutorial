a3b94c5213b8ea994c33101b94d23dba14ed97fe
j-daniel-s <jsharper55@gmail.com>
2024-09-21 06:12:23 -0500

    completed the second set of instructions, if I understood them correctly.  Navigate to '/guySpeakingLatin/guySpeakingLatin'

--- src/pages/guySpeakingLatin/guySpeakingLatin.tsx
+++ src/pages/guySpeakingLatin/guySpeakingLatin.tsx
@@ -1,29 +1,28 @@
 import { Card, CardContent, Typography } from "@mui/material";
-import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Animal from "../../types/animal";
 
 const guySpeakingLatin = () => {
-  const [toggle, setToggle] = useState(true);
   const [latin, setLatin] = useState("");
 
-  useEffect(() => {
-    setToggle(false);
-    async function getData() {
-      await fetch('/api/guySpeakingLatin')
-      .then(response => response.json())
-      .then(json => {
-        let res = json.latin;
+  const getData = async () => {
+    try {
+        const res =  await fetch('/api/guySpeakingLatin')
+        .then(response => response.json())
+        .then(json => {
+            let res = json.latin;
         if (json.latin.title !== undefined)
             res = json.latin.title;
         console.log(res)
         setLatin(res);
-      });
-      return;
+        });
+    } finally {
+        setTimeout(getData, 3000);
     }
+}
+
+  useEffect(() => {
     getData();
-  }, [latin]);
+  }, []);
 
   return (
     <main className="main-contianer" style={{ paddingTop: "5vh" }}>
